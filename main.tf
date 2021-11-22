396a160e08dab8845bf39118e159629a188bf6ee
Diego Leon <diegoleon.77@hotmail.com>
2021-11-22 17:19:31 -0500

    Agregando grupo de seguridad

--- main.tf
+++ main.tf
@@ -1,22 +1,33 @@
-terraform {
-  required_version = "> 0.7.0"
-}
-
+#Selección del proveedor
 provider "aws" {
   region = "us-east-1"
 }
 
+#Creación de servidor web
 resource "aws_instance" "project-instance" {
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
   ami               = "ami-083654bd07b5da81d"
-  
-  tags = {
-    Name = "terraform-project"
-  }
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo service apache2 start
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
               EOF
+
+  tags = {
+    Name = "terraform-project"
+  }
+}
+
+#Grupo de seguridad para permitir el tráfico entrante o saliente
+resource "aws_security_group" "instance" {
+  name = "terraform-project-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
