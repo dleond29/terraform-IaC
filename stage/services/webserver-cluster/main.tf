99aad846d3704c279318f6c2846f30f949405924
Diego Leon <diegoleon.77@hotmail.com>
2021-11-23 12:58:15 -0500

    Implementacion sin tiempo de inactividad

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
+  ami         = "ami-083654bd07b5da81d"
+  server_text = "New server text v2"
+
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-myproject-state"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
@@ -12,6 +15,7 @@ module "webserver_cluster" {
   instance_type = "t2.micro"
   min_size      = 1
   max_size      = 1
+  
 }
 
 #Exponer un puerto adicional, solo para realizar pruebas
